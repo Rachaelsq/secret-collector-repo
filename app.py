192a614320297d98f1776fa58ea83e01b2eb1f97
Rachaelsq <rachaelqsimone@gmail.com>
2021-12-30 20:49:56 -0600

    add rows to db
    able to add email row to db however secret is null

--- app.py
+++ app.py
@@ -5,6 +5,26 @@ import sqlalchemy
 import sys
 
 app=Flask(__name__)
+#postgres database url
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:postgres123@localhost/secret_collector'
+db=SQLAlchemy(app)
+
+
+''' 
+================
+MODEL
+================
+'''
+class Data(db.Model):
+    __tablename__="data"
+    id = db.Column(db.Integer, primary_key=True)
+    email_ = db.Column(db.String(120), unique=True)
+    secret_ = db.Column(db.String)
+
+    def __init__(self, email_, secret_):
+        self.email_=email_
+        self.secret=secret_
+
 
 
 ''' 
@@ -23,6 +43,11 @@ def success():
         email=request.form["email_name"]
         secret=request.form["secret_text"]
         print(request.form)
+        #create object instance of data class
+        data=Data(email, secret)
+        #commit new row/changes to database
+        db.session.add(data)
+        db.session.commit()
     return render_template("success.html")
 
 
